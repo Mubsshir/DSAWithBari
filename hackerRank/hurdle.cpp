2379a170b2245b5a5bb4b93193ceb16b1f9321a7
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2023-05-15 02:30:46 +0530

    greedy algo

---
+++ hackerRank/hurdle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+int main()
+{
+  std::vector<int> nums = {2,3,1,1,4};
+  int count = 0, i = 0, jumpsTo = 0;
+  int hurdlesCount = nums.size();
+   while (i < hurdlesCount)
+  {
+    jumpsTo = (jumpsTo == 0) ? nums[i] : jumpsTo + nums[i];
+
+    if (jumpsTo >= hurdlesCount)
+    {
+      count++;
+      break;
+    }
+
+    if (nums[jumpsTo] == 0)
+    {
+      count = -1;
+      break;
+    }
+
+    i = jumpsTo;
+    count++;
+  }
+  std::cout << count;
+}
---
+++ hackerRank/main.js
@@ -0,0 +1,26 @@
+const countJumps=(track)=>{
+  let i=jumpsTo=count=0;
+  let trackLength=track.length;
+  while(i<trackLength){
+    jumpsTo+=track[i];
+    if(jumpsTo>=trackLength){
+      count++;
+      break;
+    }
+    if(track[jumpsTo]===0){
+      count=-1;
+      break;
+    }
+    if(jumpsTo<trackLength){
+      count++;
+      i=jumpsTo;
+      continue;
+    }
+    break;
+  }
+  return count;
+}
+
+let a=[2,3,1,1,4];
+
+console.log(countJumps(a));
