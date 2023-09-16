946c886136583107b8d44f0b1fa6bc4d3fa15be1
Mubsshir <khanmubsshir@gmail.com>
2023-09-16 16:31:11 +0530

    added matrix multiplication code

--- matrix.hpp
+++ matrix.hpp
@@ -1,60 +1,61 @@
 #include <iostream>
 using namespace std;
 
-class Matrix
-{
+class Matrix {
 public:
   int m, n;
   int *matrix;
 
-  Matrix()
-  {
-    this->matrix = new int[4];
-  }
-  Matrix(int m, int n)
-  {
+  Matrix() { this->matrix = new int[4]; }
+  Matrix(int m, int n) {
     this->m = m;
     this->n = n;
     this->matrix = new int[m * n];
   }
-  ~Matrix()
-  {
-    delete[] matrix;
-  }
+  ~Matrix() { delete[] matrix; }
   void Show();
   void SetElement(int i, int j, int x);
   int GetElement(int i, int j);
   static Matrix Multiply(Matrix a, Matrix b);
 };
 
-void Matrix::SetElement(int i, int j, int x)
-{
-  if (i <= this->n && j <= this->n)
-  {
+void Matrix::SetElement(int i, int j, int x) {
+  if (i <= this->n && j <= this->n) {
     this->matrix[3 * i + j - 4] = x;
     return;
   }
-  cout << "Out of array boundary";
+  throw out_of_range("Out of array boundary");
 }
 
-int Matrix::GetElement(int i, int j)
-{
-  if (i <= this->n && j <= this->n)
-  {
+int Matrix::GetElement(int i, int j) {
+  if (i <= this->n && j <= this->n) {
     return this->matrix[3 * i + j - 4];
   }
-  cout << "Out of array boundary";
-  return;
+  throw out_of_range("Out of array boundary");
 }
 
-void Matrix::Show()
-{
-  for (int i = 1; i < this->m + 1; i++)
-  {
-    for (int j = 1; j < this->n + 1; j++)
-    {
-      printf("%d  ",GetElement(i,j));
+void Matrix::Show() {
+  cout << "\n";
+  for (int i = 1; i <= this->m; i++) {
+    for (int j = 1; j <= this->n; j++) {
+      printf("%d  ", GetElement(i, j));
     }
-    cout<<"\n\n";
+    cout << "\n\n";
   }
+}
+
+Matrix Matrix::Multiply(Matrix a, Matrix b) {
+  Matrix c(b.m, b.n);
+  if (a.n != b.m)
+    throw range_error("\nColumn of 1st matrix should be equal to rows of 2nd matrix.");
+  for (int i = 1; i <= a.n; i++) {
+    for (int j = 1; j <= b.n; j++) {
+      int sum = 0;
+      for (int k = 1; k <= b.m; k++) {
+        sum += a.GetElement(i, k) * b.GetElement(k, j);
+      }
+      c.SetElement(i, j, sum);
+    }
+  }
+  return c;
 }
